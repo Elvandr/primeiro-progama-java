dc57f89e5b40b1f3239d0cd13fa09e5c3f8bd60d
Elvandr <juninhosilveira231@gmail.com>
2023-01-31 14:46:03 -0300

    Initial commit

---
+++ src/primeiroclassejava.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class primeiroclassejava {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("meu primeiro progama em java ");
+
+
+    }
+}
